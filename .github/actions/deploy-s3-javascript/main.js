aeed944141ce7d4a04dc33c72f86235193a64c6b
Vitalii Mahdych <vitalii.mahdych@gmail.com>
2022-11-28 15:42:46 +0200

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,9 @@ function run() {
   
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
-  core.notice('Hello from my custom Java Script Action!');
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+  core.setOutput('website-url', websiteUrl)  //::set-output
+
 }
 
 run();
