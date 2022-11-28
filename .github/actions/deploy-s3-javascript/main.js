095b560886d9ee7e81be89aa80eeba59bcbfb66b
Vitalii Mahdych <vitalii.mahdych@gmail.com>
2022-11-28 15:25:47 +0200

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,19 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-  core.notice('Hello from my custom Java Script Action!')
+  // 1) Get some inout values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: false });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`
+  
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
+  core.notice('Hello from my custom Java Script Action!');
 }
 
 run();
